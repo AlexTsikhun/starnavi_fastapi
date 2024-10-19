d3643dc57efa990bdbfacb394bbdc5738579415a
Oleksadr Tsikhun <sasacihun@gmail.com>
2024-10-19 12:37:45 +0300

    feat: Create profanity_checker з Gemini API

---
+++ gemini.py
@@ -0,0 +1,16 @@
+import os
+
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+
+def profanity_checker(text):
+    prompt = f"Оціни, чи даний текст містить ненормативну лексику: {text}. Відповідай числом, 1 - якщо вона є чи 0 якщо немає"
+    response = model.generate_content(prompt)
+    return response.text
