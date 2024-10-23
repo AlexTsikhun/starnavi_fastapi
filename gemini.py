01a95936b09acdda0e289f51fdd3c34168e04338
Oleksadr Tsikhun <sasacihun@gmail.com>
2024-10-23 12:09:14 +0300

    Fix Gemini prompts to better output

--- gemini.py
+++ gemini.py
@@ -11,7 +11,11 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 
 
 def profanity_checker(text):
-    prompt = f"Оціни, чи даний текст містить ненормативну лексику: {text}. Відповідай числом, 1 - якщо вона є чи 0 якщо немає"
+    prompt = (
+        f"Оціни, чи даний текст містить ненормативну лексику: {text}."
+        f"Якщо тобі недостатньо контексту, то найбільш імовірно, текст не містить ненормативну лексику"
+        f" Відповідай числом, 1 - якщо вона є чи 0 якщо немає"
+    )
     response = model.generate_content(prompt)
     return int(response.text)
 
@@ -19,10 +23,10 @@ def profanity_checker(text):
 def gemini_auto_reply(post_content, comment_content):
     prompt = (
         f"I published a social media post with the following content '{post_content}'"
-        f" and received a comment. I want you to respond to the comment, taking "
-        f"into account its content and emotions. Your goal is to be friendly, "
-        f"and provide helpful responses. "
-        f"Here's the comment: '{comment_content}'. Answer in the language in which the question was asked "
+        f" and received a '{comment_content}'. I want you to respond to the comment,"
+        f" don't ask questions back. Your goal is to be friendly."
+        f"If you have a problem with a understanding, just write 'thank you'."
+        f"Answer in the language in which the question was asked "
     )
     response = model.generate_content(prompt)
     return response.text
